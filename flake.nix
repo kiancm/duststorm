d5ebc8d9cfba9512aefec829293f85dabc853145
Kian Mehrabani <kianmehrabani@gmail.com>
2024-03-22 17:09:59 -0400

    format shell scripts

--- flake.nix
+++ flake.nix
@@ -23,25 +23,45 @@
           (pkgs.writeShellApplication {
             name = "echo-test";
             text = ''
-              cargo build && maelstrom test -w echo --bin target/debug/echo --node-count 1 --time-limit 10
+              cargo build && maelstrom test \
+              -w echo \
+              --bin target/debug/echo \
+              --node-count 1 \
+              --time-limit 10
             '';
           })
           (pkgs.writeShellApplication {
             name = "unique-ids-test";
             text = ''
-              cargo build && maelstrom test -w unique-ids --bin target/debug/unique_ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+              cargo build && maelstrom test \
+              -w unique-ids \
+              --bin target/debug/unique_ids \
+              --time-limit 30 \
+              --rate 1000 \
+              --node-count 3 \
+              --availability total \
+              --nemesis partition
             '';
           })
           (pkgs.writeShellApplication {
             name = "broadcast-single-test";
             text = ''
-              cargo build && maelstrom test -w broadcast --bin target/debug/broadcast --node-count 1 --time-limit 10
+              cargo build && maelstrom test \
+              -w broadcast \
+              --bin target/debug/broadcast \
+              --node-count 1 \
+              --time-limit 10
             '';
           })
           (pkgs.writeShellApplication {
             name = "broadcast-test";
             text = ''
-              cargo build && maelstrom test -w broadcast --bin target/debug/broadcast --node-count 5 --time-limit 20 --rate 10
+              cargo build && maelstrom test \
+                -w broadcast \
+                --bin target/debug/broadcast \
+                --node-count 5 \
+                --time-limit 20 \
+                --rate 10
             '';
           })
         ];

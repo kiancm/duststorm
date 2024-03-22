4936b191650a4142717e054a70ef418280a2994f
Kian Mehrabani <kianmehrabani@gmail.com>
2024-03-22 16:11:21 -0400

    add unique-id test script

--- flake.nix
+++ flake.nix
@@ -27,6 +27,12 @@
             '';
           })
           (pkgs.writeShellApplication {
+            name = "unique-ids-test";
+            text = ''
+              cargo build && maelstrom test -w unique-ids --bin target/debug/unique_ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+            '';
+          })
+          (pkgs.writeShellApplication {
             name = "broadcast-single-test";
             text = ''
               cargo build && maelstrom test -w broadcast --bin target/debug/broadcast --node-count 1 --time-limit 10
